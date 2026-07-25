4813c6e04f0ab6390213702b0c349c9b290fd87e
admin <admin@example.com>
2026-07-25 14:53:30 +0800

    fix: 更新vite base路径适配GitHub Pages子路径部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: './',
+  base: '/chakan/',
   build: {
     sourcemap: 'hidden',
     assetsInlineLimit: 0,
